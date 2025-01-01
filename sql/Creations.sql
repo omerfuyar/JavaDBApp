370c8520f772164f00965505a5964b41585f2c6d
omerfuyar <denemeyo@gmail.com>
2025-01-01 04:57:51 +0200

    folder and file system, some sql commands

---
+++ sql/Creations.sql
@@ -0,0 +1,69 @@
+select *
+  from dual; --test connection
+
+create user omruyr identified by 078864; --create user
+
+grant connect,resource to omruyr; --grant connect and resource roles to user
+
+CONNECT omruyr/078864@localhost:1521/XEPDB1; --switch / connect to user
+
+create table aircrafts (
+   ac_id                number(10) primary key,
+   ac_model             varchar2(100) unique not null,
+   ac_class             varchar2(100),
+   ac_country           varchar2(100) not null,
+   ac_company           varchar2(100) not null,
+   ac_status            varchar2(100) not null,
+   ac_units             number(10) check ( ac_units > 0 ),
+   ac_first_flight_year number(4) check ( ac_first_flight_year > 0 ),
+   ac_wheight           number(10) check ( ac_wheight > 0 ),
+   ac_length            number(10) check ( ac_length > 0 )
+);
+
+create sequence ac_id_seq start with 100000 increment by 1 maxvalue 999999;
+
+create table countries (
+   country_name   varchar2(100) primary key,
+   country_region varchar2(100) not null
+);
+
+create table regions (
+   region_name varchar2(100) primary key
+);
+
+create table companies (
+   company_name    varchar2(100) primary key,
+   company_country varchar2(100) not null
+);
+
+create table classes (
+   class_name varchar2(100) primary key
+);
+
+create table statuses (
+   status_name varchar2(100) primary key
+);
+
+alter table aircrafts
+   add constraint fk_ac_country foreign key ( ac_country )
+      references countries ( country_name );
+
+alter table aircrafts
+   add constraint fk_ac_company foreign key ( ac_company )
+      references companies ( company_name );
+
+alter table aircrafts
+   add constraint fk_ac_class foreign key ( ac_class )
+      references classes ( class_name );
+
+alter table aircrafts
+   add constraint fk_ac_status foreign key ( ac_status )
+      references statuses ( status_name );
+
+alter table countries
+   add constraint fk_country_region foreign key ( country_region )
+      references regions ( region_name );
+
+alter table companies
+   add constraint fk_company_country foreign key ( company_country )
+      references countries ( country_name );
--- src/App.java
+++ src/App.java
@@ -4,7 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class App {
-    static String query = "SELECT * FROM dual";
+    static String query = "SELECT * FROM countries";
 
     public static void main(String[] args) throws Exception {
         // System.out.println("Hello, World!");
@@ -21,9 +21,11 @@ public class App {
             Connection connection = DriverManager.getConnection(url, userName, password);
             PreparedStatement statement = connection.prepareStatement(query);
             ResultSet resultSet = statement.executeQuery(query);
-            resultSet.next();
-            String result = resultSet.getString(1);
-            System.out.println(result);
+            while (resultSet.next()) {
+                String result1 = resultSet.getString(1);
+                String result2 = resultSet.getString(2);
+                System.out.println(result1 + "    " + result2);
+            }
 
         } catch (Exception e) {
             System.out.println(e);
